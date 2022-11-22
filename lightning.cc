8c0491f7b2db2c142dc4e616b0cdfc0ceb884428
Justin Green <electrojustin@gmail.com>
2022-11-22 14:48:58 -0500

    Improved lighting by adding changing directionality to the traces

--- lightning.cc
+++ lightning.cc
@@ -6,6 +6,7 @@
 #include <png.h>
 #include <thread>
 #include <vector>
+#include <math.h>
 
 #include "qt_display.h"
 #include "markov.h"
@@ -21,18 +22,87 @@ struct Coord {
   int y;
 };
 
+class Lead {
+public:
+  Coord coord;
+  double heading_angle;
+  std::vector<uint32_t> pdf;
+  std::unique_ptr<MarkovSampler> walk_sampler;
+
+  Lead(Coord coord, double heading_angle);
+  Lead(Lead&& lead);
+};
+
+void vector_pdf_helper(int& dir1, int& dir2, int vector_component, int amplitude) {
+  if (vector_component >= 0) {
+    dir1 = amplitude/2 + vector_component/2;
+    dir2 = amplitude - dir1;
+  } else {
+    dir2 = amplitude/2 - vector_component/2;
+    dir1 = amplitude - dir2;
+  }
+}
+
+Lead::Lead(Coord coord, double heading_angle) {
+  this->coord = coord;
+  this->heading_angle = heading_angle;
+
+  int vec_x, vec_y, ortho_vec_x, ortho_vec_y;
+  vec_x = (int)(sin(heading_angle) * 100.0);
+  vec_y = (int)(cos(heading_angle) * 100.0);
+  ortho_vec_x = -1*vec_y;
+  ortho_vec_y = vec_x;
+
+  int left, right, up, down;
+  vector_pdf_helper(right, left, vec_x, 100);
+  vector_pdf_helper(down, up, vec_y, 100);
+
+  int left_noise, right_noise, up_noise, down_noise;
+  vector_pdf_helper(right_noise, left_noise, ortho_vec_x, 100);
+  vector_pdf_helper(down_noise, up_noise, ortho_vec_y, 100);
+  left += left_noise;
+  right += right_noise;
+  up += up_noise;
+  down += down_noise;
+  ortho_vec_x *= -1;
+  ortho_vec_y *= -1;
+  vector_pdf_helper(right_noise, left_noise, ortho_vec_x, 100);
+  vector_pdf_helper(down_noise, up_noise, ortho_vec_y, 100);
+  left += left_noise;
+  right += right_noise;
+  up += up_noise;
+  down += down_noise;
+
+//  printf("angle: %f\n", heading_angle);
+//  printf("vec_x: %d, vec_y: %d\n", vec_x, vec_y);
+//  printf("down: %d, left: %d, right: %d, up: %d\n", down, left, right, up);
+
+  pdf.push_back(down);
+  pdf.push_back(left);
+  pdf.push_back(right);
+  pdf.push_back(up);
+  walk_sampler = std::make_unique<MarkovSampler>(pdf);
+}
+
+Lead::Lead(Lead&& lead) {
+  coord = lead.coord;
+  heading_angle = lead.heading_angle;
+  pdf = std::move(lead.pdf);
+  walk_sampler = std::move(lead.walk_sampler);
+}
+
 class Bolt {
 private:
   std::vector<Coord> trace;
-  std::vector<Coord> leads;
-  static const uint32_t trace_color = 0xFF886666;
+  std::vector<Lead> leads;
+  static const uint32_t trace_color = 0xFF444488;
   uint32_t flash_color = 0xFFFFFFFF;
-  const std::vector<uint32_t> walk_pdf = {10, 10, 10, 1};
   const std::vector<uint32_t> trace_split_pdf = {1000, 1};
-  std::unique_ptr<MarkovSampler> walk_sampler = std::make_unique<MarkovSampler>(walk_pdf);
   std::unique_ptr<MarkovSampler> trace_split_sampler = std::make_unique<MarkovSampler>(trace_split_pdf);
+  const std::vector<uint32_t> split_dir_pdf = {1, 1};
+  std::unique_ptr<MarkovSampler> split_dir_sampler = std::make_unique<MarkovSampler>(split_dir_pdf);
   bool is_flashing = false;
-  static const int flash_decay = 10;
+  static const int flash_decay = 1;
 
 public:
   Bolt(Coord seed);
@@ -44,14 +114,15 @@ public:
 };
 
 Bolt::Bolt(Coord seed) {
-  leads.push_back(seed);
+  Lead primary(seed, 0.0);
+  leads.emplace_back(std::move(primary));
 }
 
 Bolt::Bolt(Bolt&& bolt) {
   trace = std::move(bolt.trace);
   leads = std::move(bolt.leads);
-  walk_sampler = std::move(bolt.walk_sampler);
   trace_split_sampler = std::move(bolt.trace_split_sampler);
+  split_dir_sampler = std::move(split_dir_sampler);
   flash_color = bolt.flash_color;
   is_done = bolt.is_done;
   is_flashing = bolt.is_flashing;
@@ -73,42 +144,48 @@ void Bolt::process() {
     return;
   }
 
-  std::vector<Coord> new_leads;
-  for (Coord& lead : leads) {
-    trace.emplace_back(lead);
+  std::vector<Lead> new_leads;
+  for (Lead& lead : leads) {
+    trace.emplace_back(lead.coord);
 
-    switch (walk_sampler->sample()) {
+    switch (lead.walk_sampler->sample()) {
       case 0:
-        lead.y++;
+        lead.coord.y++;
         break;
       case 1:
-        lead.x--;
+        lead.coord.x--;
         break;
       case 2:
-        lead.x++;
+        lead.coord.x++;
         break;
       default:
-        lead.y--;
+        lead.coord.y--;
         break;
     }
 
-    if (lead.y < 0)
-      lead.y = 0;
-    if (lead.x < 0)
-      lead.x = 0;
-    if (lead.x >= width)
-      lead.x = width-1;
-    if (lead.y >= height)
-      lead.y = height-1;
-
-    if (trace_split_sampler->sample())
-      new_leads.emplace_back(lead);
-
-    if (lead.y == height-1)
+    if (lead.coord.y < 0) {
+      lead.coord.y = 0;
       is_flashing = true;
+    } else if (lead.coord.x < 0) {
+      lead.coord.x = 0;
+      is_flashing = true;
+    } else if (lead.coord.x >= width) { 
+      lead.coord.x = width-1;
+      is_flashing = true;
+    } else if (lead.coord.y >= height) {
+      lead.coord.y = height-1;
+      is_flashing = true;
+    } else if (trace_split_sampler->sample()) {
+      int dir = split_dir_sampler->sample();
+      //printf("dir: %d\n", dir);
+      float new_heading_angle = dir ? lead.heading_angle + M_PI_4 : lead.heading_angle - M_PI_4;
+      Lead new_lead(lead.coord, new_heading_angle);
+      new_leads.emplace_back(std::move(new_lead));
+    }
   }
 
-  leads.insert(leads.end(), new_leads.begin(), new_leads.end());
+  for (Lead& lead : new_leads)
+    leads.emplace_back(std::move(lead));
 }
 
 void Bolt::render() {
@@ -122,33 +199,45 @@ void Bolt::render() {
     }
   }
 
-  for (Coord& lead : leads) {
-    color_buf[lead.y * width + lead.x] = flash_color;
+  for (Lead& lead : leads) {
+    color_buf[lead.coord.y * width + lead.coord.x] = flash_color;
   }
 }
 
+void process_bolt(std::vector<Bolt>* bolts, int idx) {
+  for (int i = 0; i < 10; i++)
+    (*bolts)[idx].process();
+
+  (*bolts)[idx].render();
+}
 
 void paint_loop() {
   auto last_buf_swap = std::chrono::high_resolution_clock::now();
   int kRefreshPeriod = 33000;
-  std::vector<uint32_t> new_bolt_pdf = {250, 1};
+  std::vector<uint32_t> new_bolt_pdf = {150, 1};
   MarkovSampler new_bolt_sampler(new_bolt_pdf);
   std::vector<Bolt> bolts;
   const int flash_decay = 1;
+  bool first_frame = true;
   while(1) {
-    if (new_bolt_sampler.sample()) {
+    if (first_frame || new_bolt_sampler.sample()) {
       Coord seed_coord;
-      printf("New bolt!\n");
+      //printf("New bolt!\n");
       seed_coord.y = 0;
       seed_coord.x = (rand() % (width - 2)) + 1;
       bolts.emplace_back(std::move(Bolt(seed_coord)));
+      first_frame = false;
     }
 
     std::vector<Bolt> next_cycle_bolts;
+    std::vector<std::shared_ptr<std::thread>> bolt_threads;
+    for (int i = 0; i < bolts.size(); i++)
+      bolt_threads.emplace_back(std::make_shared<std::thread>(process_bolt, &bolts, i));
+
+    for (auto thread : bolt_threads)
+      thread->join();
+
     for (Bolt& bolt : bolts) {
-      for (int i = 0; i < 10; i++)
-        bolt.process();
-      bolt.render();
       if (!bolt.is_done)
         next_cycle_bolts.emplace_back(std::move(bolt));
     }
